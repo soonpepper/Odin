90269f64e002df3dbdec4c34ab238796dfa591c7
soonpepper <vii.mjay@gmail.com>
2022-12-25 17:14:39 -0500

    js practice

---
+++ js-practice/stuff.js
@@ -0,0 +1,20 @@
+/* js */
+
+function pleaseLoveMe(name) {
+    return "I love you " + name;
+}
+
+let answer = parseInt(prompt("Enter number: "));
+
+for(i = 0; i < answer; i++){
+    if (i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+
+}
